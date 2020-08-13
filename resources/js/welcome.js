14b1459fb0e13dd2e9a02d7b4eb7edd4f74a9406
kaisei <kaisei101010@icloud.com>
2020-08-13 20:45:46 +0900

    ハンバーガーメニューaリンク、クリックで自動閉じ、thankspageデザイン追加

--- resources/js/welcome.js
+++ resources/js/welcome.js
@@ -1,7 +1,7 @@
 import Typed from "typed.js/src/typed";
 
 new Typed('.typed', {
-  strings: ["Welcome to My Portfolio.", "Let's scroll down!."],
+  strings: ["Welcome to My Portfolio.", "Let's scroll down."],
   typeSpeed: 40,
   startDelay: 2700,
   backspeed: 80,
@@ -26,6 +26,9 @@ $(function() {
         }
     });
 });
+$('.globalMenuSp a[href]').on('click', function() {
+        $('.globalMenuSp,.hamburger').removeClass('active');
+    });
 $(document).ready(function() {
             $('.bxslider').bxSlider({
 
@@ -51,7 +54,7 @@ $(document).ready(function() {
             });
         });
 
-var sheader = $("header"),
+let sheader = $("header"),
     animationClass = "is-animation";
 
 $(window).scroll(function () {
--- resources/views/complete.blade.php
+++ resources/views/complete.blade.php
@@ -2,11 +2,28 @@
     <head>
         <meta charset="utf-8">
         <title>KAISEI portfolio</title>
+        <link rel="stylesheet" href="/css/app.css">
+        <link rel="shortcut icon" href="img/favicon.ico">
+        <meta name="viewport" content="width=device-width">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     </head>
     <body>
-        <div>
-            Thank You!
+      <div class="thanksWrap">
+        <div class="thanksFirst">
+            <h1>Thank You!</h1>
         </div>
-        <a href="{{ route('welcome') }}">戻るzo</a>
+        <div class="checkContent">
+          <i class="fas fa-check checkColor"></i>
+          <h2>お問い合わせ 送信完了</h2>
+          <p>
+お問い合わせありがとうございました。<br>
+内容を確認のうえ、原則1~3営業日以内に返信いたします。
+しばらくお待ちください。</p>
+        </div>
+        <a href="{{ route('welcome') }}"><h4>戻る</h4></a>
+        <div class="thanksLast">
+          <p><small>© Copyright | Kaisei.Portfolio 2020.</small></p>
+        </div>
+      </div>
     </body>
 </html>
